d5d4b2ded1b100be8a9c19401acf85f468fe55ad
Biswajit Singha SDC 32 <biswasingha.09@gmail.com>
2018-07-05 15:42:33 +0530

    change on config file

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports={
-	database:'mongodb://localhost/ems'
+	//database:'mongodb://localhost/ems'
+	database:'mongodb://heroku_ll6bhl08:kokv220oc3jcjahj6o29rk1auh@ds127781.mlab.com:27781/heroku_ll6bhl08'
 }
