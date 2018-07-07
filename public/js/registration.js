7af27e2da19fbd8dfe2ae0cd199cfe02857f9584
Biswajit Singha SDC 32 <biswasingha.09@gmail.com>
2018-07-07 11:15:24 +0530

    change on manifest and sw file

--- public/js/registration.js
+++ public/js/registration.js
@@ -1,26 +1,39 @@
-	// 	//Registration of service worker
-    //     if ('serviceWorker' in navigator) {
-	// 		window.addEventListener('load', function() {
-	// 		navigator.serviceWorker.register('/sw.js').then(function(registration) {
-	// 		// Registration was successful
-	// 		console.log('ServiceWorker registration successful with scope: ', registration.scope);
-	// 		}, function(err) {
-	// 		// registration failed :(
-	// 		console.log('ServiceWorker registration failed: ', err);
-	// 	});
-	//   });
-    // }
-    
-
+	//Registration of service worker
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker
-          .register('/sw.js')
-          .then(function () {
-            console.log('Service worker registered!');
-          })
-          .catch(function(err) {
-            console.log(err);
-          });
-      }
+		window.addEventListener('load', function() {
+		navigator.serviceWorker.register('/sw.js').then(function(registration) {
+		// Registration was successful
+		console.log('ServiceWorker registration successful with scope: ', registration.scope);
+		}, function(err) {
+		// registration failed :(
+		console.log('ServiceWorker registration failed: ', err);
+	});
+  });
+}
+
+
+// var deferredPrompt;
+
+// if (!window.Promise) {
+//     window.Promise = Promise;
+// }
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker
+//         .register('/sw.js')
+//         .then(function () {
+//             console.log('Service worker registered!');
+//         })
+//         .catch(function (err) {
+//             console.log(err);
+//         });
+// }
+
+
+// window.addEventListener('beforeinstallprompt', function(event) {
+//     console.log('beforeinstallprompt fired');
+//     event.preventDefault();
+//     deferredPrompt = event;
+//     return false;
+//   });
 
 
--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,9 @@ var filesToCache = [
   '/',
   'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
   'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
-  'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js'
+  'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js',
+  '/css/adminstyle.css',
+  '/css/style.css'
 ];
 
 self.addEventListener('install', function(e) {
